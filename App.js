9c378b8e2bd5594ab0055c3ccfda4be467388f97
Danielle Torres de Freitas <danielletorree01@gmail.com>
2021-11-04 17:57:50 -0300

    melhorias do codigo

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import {
   Text,
   Dimensions,
   TouchableHighlight,
-  TouchableOpacity
+  StatusBar,
+  StyleSheet
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import colors from './src/colors.json'
@@ -25,142 +26,185 @@ import colors from './src/colors.json'
 const { width, height } = Dimensions.get("window");
 
 const App = () => {
-
+  const [rerender, setRerender] = useState(false);
+  const palavras = ['ESCOLA', 'BARCO', 'MAR', 'MUSEU'];
+  const [sentence, setSentence] = useState([]);
   const [letra, setLetra] = useState([
-    ['A', 'X', 'V', 'E', 'K', 'L'],
-    ['E', 'S', 'C', 'O', 'L', 'A'],
-    ['B', 'A', 'R', 'C', 'O', 'R'],
-    ['O', 'G', 'I', 'R', 'B', 'A'],
-    ['M', 'A', 'R', 'I', 'C', 'A'],
-    ['S', 'U', 'E', 'S', 'U', 'M']
+    [
+      { valor: 'A', selecionado: false, id: 0 },
+      { valor: 'X', selecionado: false, id: 1 },
+      { valor: 'V', selecionado: false, id: 2 },
+      { valor: 'E', selecionado: false, id: 3 },
+      { valor: 'K', selecionado: false, id: 4 },
+      { valor: 'R', selecionado: false, id: 5 }
+    ],
+    [
+      { valor: 'E', selecionado: false, id: 6 },
+      { valor: 'S', selecionado: false, id: 7 },
+      { valor: 'C', selecionado: false, id: 8 },
+      { valor: 'O', selecionado: false, id: 9 },
+      { valor: 'L', selecionado: false, id: 10 },
+      { valor: 'A', selecionado: false, id: 11 }
+    ],
+    [
+      { valor: 'O', selecionado: false, id: 12 },
+      { valor: 'G', selecionado: false, id: 13 },
+      { valor: 'I', selecionado: false, id: 14 },
+      { valor: 'R', selecionado: false, id: 15 },
+      { valor: 'B', selecionado: false, id: 16 },
+      { valor: 'R', selecionado: false, id: 17 }
+    ],
+    [
+      { valor: 'M', selecionado: false, id: 18 },
+      { valor: 'A', selecionado: false, id: 19 },
+      { valor: 'R', selecionado: false, id: 20 },
+      { valor: 'I', selecionado: false, id: 21 },
+      { valor: 'C', selecionado: false, id: 22 },
+      { valor: 'A', selecionado: false, id: 23 }
+    ],
+    [
+      { valor: 'S', selecionado: false, id: 24 },
+      { valor: 'U', selecionado: false, id: 25 },
+      { valor: 'E', selecionado: false, id: 26 },
+      { valor: 'S', selecionado: false, id: 27 },
+      { valor: 'U', selecionado: false, id: 28 },
+      { valor: 'M', selecionado: false, id: 29 }
+    ],
+    [
+      { valor: 'B', selecionado: false, id: 30 },
+      { valor: 'A', selecionado: false, id: 31 },
+      { valor: 'R', selecionado: false, id: 32 },
+      { valor: 'C', selecionado: false, id: 33 },
+      { valor: 'O', selecionado: false, id: 34 },
+      { valor: 'L', selecionado: false, id: 35 }
+    ],
   ])
 
-  const palavras = ['ESCOLA', 'BARCO', 'MAR', 'MUSEU']
+  const letraSelecionada = (letraValor, linha, coluna, id) => {
+    for (let l = 0; l < letra.length; l++) {
+      for (let c = 0; c < letra.length; c++) {
+        if (letraValor === letra[l][c].valor && l == linha && c == coluna) {
+          letra[l][c].selecionado = !letra[l][c].selecionado;
+          comparar(letraValor, linha, coluna, id);
+          return;
+        }
+      }
+    }
+    setRerender(!rerender);
+  }
+
+  const comparar = (letraValor, linha, coluna, id) => {
+    setSentence([...sentence, letraValor, linha, coluna, id]);
+    horizontal(letraValor, id)
+
+  }
+
+  const horizontal = (letraValor, id) => {
+
+  }
 
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: colors.cor_primaria
-      }}
-    >
-      <View
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <View
-          style={{
-            width: width * 0.92,
-            height: height * 0.60,
-            backgroundColor: colors.white,
-            borderRadius: 10,
-            alignItems: 'center',
-            justifyContent: 'center',
-            flexDirection: 'row',
-            flexWrap: 'wrap'
-          }}
-        >
+    <View style={styles.container}>
+      <StatusBar backgroundColor={colors.cor_primaria} />
+      <View style={styles.containerCaixaExterna}>
+        <View style={styles.containerCaixaInterna}>
           {
             letra.map((valor, linha) => (
               valor.map((letra, coluna) => (
-                <View
-                  style={{
-                    margin: -0.1
-                  }}
-                >
-                  <TouchableOpacity
-                    key={coluna}
-                    onPress={() => alert(`letra: ${letra}; linha: ${linha}, coluna: ${coluna}`)}
-                    style={{
-                      width: width * 0.15,
-                      height: height * 0.1,
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                    }}
-                  >
-                    <Text
+                <View key={coluna}>
+                  {rerender ? (
+                    <TouchableHighlight
+                      onPress={() => letraSelecionada(letra.valor, linha, coluna, letra.id)}
+                      activeOpacity={1}
+                      underlayColor={colors.cor_secundaria}
                       style={{
-                        color: colors.black,
-                        fontSize: 28,
+                        width: width * 0.15,
+                        height: height * 0.1,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        backgroundColor: letra.selecionado ? colors.cor_secundaria : '#FFF0'
                       }}
                     >
-                      {letra}
-                    </Text>
-                  </TouchableOpacity>
+                      <Text style={styles.letraBotao}>
+                        {letra.valor}
+                      </Text>
+                    </TouchableHighlight>
+                  ) : (
+                    <TouchableHighlight
+                      onPress={() => letraSelecionada(letra.valor, linha, coluna, letra.id)}
+                      activeOpacity={1}
+                      underlayColor={colors.cor_secundaria}
+                      style={{
+                        width: width * 0.15,
+                        height: height * 0.1,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        backgroundColor: letra.selecionado ? colors.cor_secundaria : '#FFF0'
+                      }}
+                    >
+                      <Text style={styles.letraBotao}>
+                        {letra.valor}
+                      </Text>
+                    </TouchableHighlight>
+                  )
+                  }
                 </View>
               ))
-
             ))
           }
         </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'center',
-            alignItems: 'center',
-            width: width * 0.8
-          }}
-        >
-          <Text
-            style={{
-              color: colors.white,
-              fontSize: 24,
-              flexWrap: 'wrap',
-              width: width * 0.5,
-              fontWeight: 'bold'
-            }}
-          >
-            {palavras[0]}
-          </Text>
-
-          <Text
-            style={{
-              color: colors.white,
-              fontSize: 24,
-              flexWrap: 'wrap',
-              width: width * 0.5,
-              fontWeight: 'bold'
-            }}
-          >
-            {palavras[1]}
-          </Text>
-        </View>
-
-        <View
-          style={{
-            flexDirection: 'row'
-          }}
-        >
-          <Text
-            style={{
-              color: colors.white,
-              fontSize: 24,
-              flexWrap: 'wrap',
-              width: width * 0.5,
-              fontWeight: 'bold'
-            }}
-          >
-            {palavras[2]}
-          </Text>
-
-          <Text
-            style={{
-              color: colors.white,
-              fontSize: 24,
-              flexWrap: 'wrap',
-              width: width * 0.5,
-              fontWeight: 'bold'
-            }}
-          >
-            {palavras[3]}
-          </Text>
-        </View>
+      </View>
+      <View style={styles.containerPalavras}>
+        <Text style={styles.palavraDoContainer}>{palavras[0]}</Text>
+        <Text style={styles.palavraDoContainer}>{palavras[1]}</Text>
+      </View>
+      <View style={styles.containerPalavras}>
+        <Text style={styles.palavraDoContainer}>{palavras[2]}</Text>
+        <Text style={styles.palavraDoContainer}>{palavras[3]}</Text>
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.cor_primaria,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  containerCaixaExterna: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: height * 0.7
+  },
+  containerCaixaInterna: {
+    width: width * 0.92,
+    height: height * 0.60,
+    backgroundColor: colors.white,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  },
+  letraBotao: {
+    color: colors.black,
+    fontSize: 28,
+    fontWeight: 'bold'
+  },
+  containerPalavras: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: width * 0.8,
+    padding: 5
+  },
+  palavraDoContainer: {
+    color: colors.white,
+    fontSize: 24,
+    fontWeight: 'bold'
+  }
+})
 
 export default App;
